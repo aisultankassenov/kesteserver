130ac303af6124671e0c0e9716767aad7350e663
aisultankassenov <aisultan.kassenov@nu.edu.kz>
2018-07-25 18:31:40 +0600

    second try4

--- server.js
+++ server.js
@@ -69,6 +69,67 @@ app.get('/schedule', function(req, res) {
 		);
 	});
 });
+app.get('/schedule2', function(req, res) {
+	var urlWithLoginForm = 'https://registrar.nu.edu.kz/';
+	var loginUrl = urlWithLoginForm + '/index.php?q=user/login';
+	var formData = { name: 'aisultan.kassenov', pass: 'Minilogo6651' };
+	var options = {
+		url: loginUrl,
+		method: 'POST',
+		form: {
+			name: req.query.name,
+			pass: req.query.pass,
+			form_id: 'user_login',
+			op: 'Log in'
+		}
+	};
+
+	request(options, function(error, response, body) {
+		var cookie = response.headers['set-cookie'];
+		var text = [];
+
+		request.get(
+			{
+				url:
+					'https://registrar.nu.edu.kz/my-registrar/personal-schedule/json?_dc=1532521851073&method=drawStudentSchedule&type=reg',
+				Cookie: cookie
+			},
+			function(err, response, body) {
+				var k = 0;
+				var $ = cheerio.load(body);
+				$('tbody')
+					.children()
+					.each(function(i, elem) {
+						text[k++] = $(this).text();
+					});
+				for (let i = 0; i < k; i++) {
+					var str = text[i];
+					text[i] = str.replace(new RegExp('<\\\\/td>', 'g'), '');
+					str = text[i];
+					text[i] = str.replace(new RegExp('\\\\r\\\\n', 'g'), '');
+					str = text[i];
+					text[i] = str.replace(new RegExp('<\\\\/i>', 'g'), '');
+					str = text[i];
+					text[i] = str.replace(new RegExp('\\\\/', 'g'), '-');
+					str = text[i];
+					text[i] = str.replace(new RegExp('<-tr>', 'g'), '');
+					str = text[i];
+					text[i] = str.replace(new RegExp('    <-span>', 'g'), '');
+					str = text[i];
+					text[i] = str.split('                            ');
+					text[i] = text[i].filter(string => string !== '');
+					text[i] = text[i].filter(
+						string =>
+							string !==
+							'  <-table>                    <-div>                <-div>                <-div>                "'
+					);
+				}
+				console.log(text);
+				res.json(text);
+			}
+		);
+	});
+});
 
 app.listen(app.get('port'), function() {
 	console.log('Magic is happening on port: ', app.get('port'));
