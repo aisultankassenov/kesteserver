d0e8fc9c55c80047f3f5bfae3cc37b5a910aeb71
aisultankassenov <aisultan.kassenov@nu.edu.kz>
2018-07-26 16:24:28 +0600

    removing unnecessary stuff

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var request = require('request');
 request = request.defaults({ jar: true });
 var cheerio = require('cheerio');
 
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/public'));
 
@@ -169,7 +169,38 @@ app.get('/schedule2', function(req, res) {
 					}
 				}
 				// console.log(text);
-				res.json(text);
+				response.json(text);
+			}
+		);
+	});
+});
+
+app.get('/exam-schedule', function(req, res) {
+	var urlWithLoginForm = 'https://registrar.nu.edu.kz/';
+	var loginUrl = urlWithLoginForm + '/index.php?q=user/login';
+	var options = {
+		url: loginUrl,
+		method: 'POST',
+		form: {
+			name: req.query.name,
+			pass: req.query.pass,
+			form_id: 'user_login',
+			op: 'Log in'
+		}
+	};
+
+	request(options, function(error, response, body) {
+		var cookie = response.headers['set-cookie'];
+		var text = [];
+
+		request.get(
+			{
+				url:
+					'https://registrar.nu.edu.kz/my-registrar/final-exam-schedule/json?method=getExams&_dc=1532599311786&page=1&start=0&limit=25',
+				cookie: cookie
+			},
+			function(err, req, res) {
+				console.log('success');
 			}
 		);
 	});
@@ -181,7 +212,7 @@ app.get('/logout', function(req, res) {
 			url: 'https://registrar.nu.edu.kz/user/logout'
 		},
 		function(req, res) {
-			console.log(res.headers);
+			console.log(res);
 		}
 	);
 });
